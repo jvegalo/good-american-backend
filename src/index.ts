9a3e1f5ec1e74560b1133ac94662473222436e2e
jvegalo <jmiguelv24@gmail.com>
2024-06-12 11:21:34 +0200

    fix: add cors policy

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import cors from "cors";
 
 const app = express();
 const port = 80;
 import favoriteProducts from "./routes/favorites";
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello, TypeScript with Express!");
